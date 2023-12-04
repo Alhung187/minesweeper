be4e79ba95b027ad0ee14992349eddfcefd81637
ValiantPhoenix13 <140972814+ValiantPhoenix13@users.noreply.github.com>
2023-12-03 22:28:22 -0800

    Update WindowMaker.java

--- src/main/java/com/mycompany/minesweeper/WindowMaker.java
+++ src/main/java/com/mycompany/minesweeper/WindowMaker.java
@@ -1,21 +1,25 @@
-package com.mycompany.minesweeper;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.minesweeper;
 
+/**
+ *
+ * @author Valiant
+ */
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.util.Random;
 import javax.swing.*;
+import java.awt.event.*;
 /**
  *
  * @author hamadkhan
@@ -35,77 +39,108 @@ public class WindowMaker extends JFrame
   
 
 public void setupLayout() {
-    
+    BombScanner scan = new BombScanner();
     Container contentPane = getContentPane();
+    contentPane.setLayout(new BorderLayout()); 
+
+        // Top panel with buttons
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JLabel numBombs = new JLabel("The number of bombs left: ");
+        JTextArea clock = new JTextArea("Time taken: ");
+        JButton button3 = new JButton("Button 3");
+        topPanel.add(numBombs);
+        topPanel.add(clock);
+        topPanel.add(button3);
     JPanel buttonPanel = new JPanel();
 
+
     //gridLayout is somethign that already makes a grid of many things (in this case buttons) in java which you can navigate
-    GridLayout gridLayout = new GridLayout(18, 18, 0, 0);
+    GridLayout gridLayout = new GridLayout(15, 15, 0, 0);
     buttonPanel.setLayout(gridLayout);
 
     //sets size of buttons so that they are squares that fit within a window the size of whatever number is being divided by 18
-    int buttonSize = 1000 / 18;
-    
+    int buttonSize = 795 / 15;
 
-    
-    ActionListener buttonListener = new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            //Started the process of checking for a right mouse click
-            //i wasn't sure how to get the button location as an input though
-            if(e.getSource() instanceof MouseEvent){ 
-                MouseEvent interaction = (MouseEvent) e.getSource();
-                if(SwingUtilities.isRightMouseButton(interaction)){
-                    rightClickButton();
-                }
-            }
-            // This is where you define what happens when a button is clicked 
-            //(preferabbly we wont code everything here we can call a class or something to run an algorith)
+    //**************************************************************************
+    //Method that adds the right click functionality to place flags
+    MouseAdapter rightClick = new MouseAdapter(){
+        public void mousePressed(MouseEvent e){
             JButton clickedButton = (JButton) e.getSource();
-            
-            int buttonIndex = buttonPanel.getComponentZOrder(clickedButton);
-            if (bombs[buttonIndex]) {
-                // Action when a bomb is clicked
-                clickedButton.setBackground(Color.RED);
-                JOptionPane.showMessageDialog(contentPane, "Boom! Game Over.");
-            } else {
-                // Action for non-bomb buttons
-                clickedButton.setEnabled(false); // disable the button
-                clickedButton.setBackground(Color.white);
+            if(SwingUtilities.isRightMouseButton(e)){
+                ImageIcon flagIcon = new ImageIcon("updatedFlag.png");
+                clickedButton.setIcon(flagIcon);
+                System.out.println("Flag placed");
             }
-
-            
         }
     };
+    //**************************************************************************
+    
+    ActionListener buttonListener = new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        JButton clickedButton = (JButton) e.getSource();
+        int buttonIndex = buttonPanel.getComponentZOrder(clickedButton);
+  
+            
+        if (bombs[buttonIndex]) {
+            // Set the bomb icon when a bomb is clicked
+            System.out.println(buttonIndex);
+            ImageIcon bombIcon = new ImageIcon("bomb.gif"); // Load the bomb icon
+            clickedButton.setIcon(bombIcon); // Set the bomb icon on the button
+            JOptionPane.showMessageDialog(contentPane, "Boom! Game Over.");
+            
+        } else {
+            // Handle non-bomb button click
+            
+            int numbombs = scan.numberOfBombs(bombs, buttonIndex);
+            String numberofbombs = numbombs+"";
+            
+            if(numbombs != 0){
+                clickedButton.setText(numberofbombs);
+            }
+            
+            clickedButton.setEnabled(false);
+            clickedButton.setBackground(Color.WHITE);
+        }
+    }
+};
+
 
    
     // loop adds all the buttons to the grid
-    for (int i = 0; i < 324; i++) {
+    for (int i = 0; i < 225; i++) {
         JButton button = new JButton();
         button.setMargin(new Insets(0, 0, 0, 0)); // this line of code is completley disposable if its still here its becasue i was trying to do something and never deleted it.
         button.setBorderPainted(false);
         button.setPreferredSize(new Dimension(buttonSize, buttonSize));
         button.setOpaque(true);
 
-        int row = i / 18;
-        int col = i % 18;
+        int row = i / 15;
+        int col = i % 15;
 
         if ((row + col) % 2 == 0) {
-            button.setBackground(Color.GRAY);// :) I got lazy so i used basic java colors if yall want we can add icons with nice textures.
+            button.setBackground(Color.DARK_GRAY);// :) I got lazy so I used basic java colors if yall want we can add icons with nice textures.
         } else {
-            button.setBackground(Color.GRAY.brighter());
+            button.setBackground(Color.gray);
         }
 
         // attaches the ActionListener to the button
         button.addActionListener(buttonListener);
+        //**********************************************************************
+        //Added a right mouse click function to the buttons
+        button.addMouseListener(rightClick);
+        //**********************************************************************   
+        
 
         buttonPanel.add(button);
     }
 
-    FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
-    contentPane.setLayout(flowLayout);
-    contentPane.add(buttonPanel);
+
+        contentPane.add(topPanel, BorderLayout.NORTH);
+        contentPane.add(buttonPanel, BorderLayout.CENTER);
+    
     
     pack(); //This will make the window the size of all the content so we dont need a default window size.
     
@@ -113,24 +148,15 @@ public void setupLayout() {
 
 private void distributeBombs() {
     Random rand = new Random();
-    int bombCount = 50; // Total number of bombs
+    int bombCount = 30; // Total number of bombs
     for (int i = 0; i < bombCount; i++) {
         int position;
         do {
-            position = rand.nextInt(324);
-        } while (bombs[position]); // make  qZq sure no bomb is placed twice at the same position
+            position = rand.nextInt(225);
+        } while (bombs[position]); // makes sure no bomb is placed twice at the same position
         bombs[position] = true;
     }
 }
-//Method to place a flag
-//Not sure if there should be an input or not...
-private void rightClickButton(){
-
-}
 
 
-
-
-    
-    
 }
